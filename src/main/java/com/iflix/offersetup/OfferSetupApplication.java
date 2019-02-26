961efdaef6fa88f7b55ed1e1e95f931d098d27db
Ebenezer Olanrewaju <ebenezer.olanrewaju@venturegardengroup.com>
2019-02-26 20:20:53 +0100

    Added spring boot

---
+++ src/main/java/com/iflix/offersetup/OfferSetupApplication.java
@@ -0,0 +1,13 @@
+package com.iflix.offersetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferSetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iflix/offersetup/OfferSetupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iflix.offersetup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OfferSetupApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
